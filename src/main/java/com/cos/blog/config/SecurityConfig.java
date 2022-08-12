c44feb89026a22ea0ad4a7851a7fcbf4fbbb6b91
mongsik2 <104689908+mongsik2@users.noreply.github.com>
2022-08-13 07:49:35 +0900

    security 로그인

--- src/main/java/com/cos/blog/config/SecurityConfig.java
+++ src/main/java/com/cos/blog/config/SecurityConfig.java
@@ -1,12 +1,18 @@
 package com.cos.blog.config;
 
+import com.cos.blog.config.auth.PrincipalDetailService;
+import com.cos.blog.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration // 빈 등록(IoC관리)
@@ -14,14 +20,27 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableGlobalMethodSecurity(prePostEnabled = true) // 특정 주소로 접근을 하면 권한 및 인증을 미리 체크
 public class SecurityConfig {
 
+    @Autowired
+    private PrincipalDetailService principalDetailService;
+
     @Bean // IoC가 됨
     public BCryptPasswordEncoder encodePWD(){
         return new BCryptPasswordEncoder();
     };
 
+
+
+
+
     // 빈 등록 : 스프링 컨테이너에서 객체를 관리할 수 있게하는 것.
     @Bean
     protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        // 시큐리티가 대신 로그인해주기 때문에 password를 가로채기를 하는데
+        // 해당 password가 뭘로 해쉬가 되어 회원가입이 되었는지를 알아야
+        // 같은 해쉬로 암호화해서 DB에 있는 해쉬랑 비교할 수 있음.
+        AuthenticationManagerBuilder auth = http.getSharedObject(AuthenticationManagerBuilder.class);
+        auth.userDetailsService(principalDetailService).passwordEncoder(encodePWD());
 
         http
                 .csrf().disable() // csrf 토큰 비활성화 (테스트시 걸어두는게 좋음)
@@ -29,7 +48,9 @@ public class SecurityConfig {
                         .antMatchers("/", "/auth/**", "/js/**", "/css/**", "/image/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .formLogin().loginPage("/auth/loginForm");
+                .formLogin().loginPage("/auth/loginForm")
+                .loginProcessingUrl("/auth/loginProc") // 스프링 시큐리티가 해당 주소로 오는 요청을 가로채서 대신 로그인
+                .defaultSuccessUrl("/"); // 로그인 성공시 이동 경로 "/"
 
 
         return http.build();
--- src/main/java/com/cos/blog/repository/UserRepository.java
+++ src/main/java/com/cos/blog/repository/UserRepository.java
@@ -5,17 +5,21 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 // DAO
 // 자동으로 bean등록이 된다.
 // @Repository // 생략이 가능하다
 public interface UserRepository extends JpaRepository<User, Integer> {
-    // JPA 네이밍 쿼리
-    // SELECT * FROM user WHERE username =?1 AND password =?2;
-    // 전통 로그인 쓰는법
-    //User findByUsernameAndPassword(String username, String password);
+    // SELECT * FROM user WHERE username =1?;
+    Optional<User> findByUsername(String username);
+}
+
+// JPA 네이밍 쿼리
+// SELECT * FROM user WHERE username =?1 AND password =?2;
+// 전통 로그인 쓰는법
+//User findByUsernameAndPassword(String username, String password);
 
     /*// 같은거
     @Query(value="SELECT * FROM user WHERE username =?1 AND password =?2", nativeQuery = true)
     User login(String username, String password);*/
-
-}
