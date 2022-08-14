b677e9800ff7ec30ec7c8e7e95ce9485ba07122e
mongsik2 <104689908+mongsik2@users.noreply.github.com>
2022-08-14 09:39:38 +0900

    글목록보기

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -1,17 +1,25 @@
 package com.cos.blog.controller;
 
 import com.cos.blog.config.auth.PrincipalDetails;
+import com.cos.blog.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BoardController {
 
+    @Autowired
+    private BoardService boardService;
+
+    // 컨트롤러에서 세션을 어떻게 찾을지
     //@AuthenticationPrincipal PrincipalDetails principalDetails
     @GetMapping({"", "/"})
-    public String index(){ // 컨트롤러에서 세션을 어떻게 찾을지
-        return "index";
+    public String index(Model model){
+        model.addAttribute("boards", boardService.글목록());
+        return "index"; // viewResolver 작동!!
     }
 
     // USER 권한 필요
--- src/main/java/com/cos/blog/service/BoardService.java
+++ src/main/java/com/cos/blog/service/BoardService.java
@@ -10,6 +10,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 public class BoardService {
 
@@ -22,6 +24,10 @@ public class BoardService {
         board.setCount(0);
         board.setUser(user);
         boardRepository.save(board);
+    }
+
+    public List<Board> 글목록() {
+        return boardRepository.findAll();
     }
 
 
