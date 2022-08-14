5992a1c304e3350626083475b9c4dbce9f686df7
mongsik2 <104689908+mongsik2@users.noreply.github.com>
2022-08-14 18:38:16 +0900

    글 상세보기

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class BoardController {
@@ -24,6 +25,13 @@ public class BoardController {
         model.addAttribute("boards", boardService.글목록(pageable));
         return "index"; // viewResolver 작동!!
     }
+
+    @GetMapping({"/board/{id}"})
+    public String findById(@PathVariable int id, Model model){
+        model.addAttribute("board",boardService.글상세보기(id));
+        return "board/detail";
+    }
+
 
     // USER 권한 필요
     @GetMapping("/board/saveForm")
