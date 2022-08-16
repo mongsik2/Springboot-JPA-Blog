9e39c737245204c39e423001d506ffb2d89e87ac
mongsik2 <104689908+mongsik2@users.noreply.github.com>
2022-08-16 10:56:21 +0900

    카카오 로그인2

--- src/main/java/com/cos/blog/service/UserService.java
+++ src/main/java/com/cos/blog/service/UserService.java
@@ -42,9 +42,9 @@ public class UserService {
             String rawPassword = user.getPassword();
             String encPassword = encoder.encode(rawPassword);
             persistence.setPassword(encPassword);
+            persistence.setEmail(user.getEmail());
         }
 
-        persistence.setEmail(user.getEmail());
 
         // 회원 수정 종료시 = 서비스 종료 = 트랜잭션 종료 = commit이 자동.
         // 영속화된 persistence 객체의 변화가 감지되면 더티체킹이 되어 update문을 날려줌.
